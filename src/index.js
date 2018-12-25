4dcba4091da2f8507eaa942b7fd3bc852ec41e16
zengbs <zengbs@cengbinhandeMBP.lan>
2018-12-25 08:56:28 +0800

    poi

--- src/index.js
+++ src/index.js
@@ -7,13 +7,65 @@ import 'wired-card';
 import 'wired-textarea';
 
 let dataChannelSendBtn = document.querySelector('#data-channel-send');
-let callBtn = document.querySelector('#call-btn');
 let loginUsersBox = document.querySelector('#login-users');
+let messageContainer = document.querySelector('#input-message');
 const wsServer = 'ws://192.168.11.136:9501';
 let webSocket = new WebSocket(wsServer);
+const name = Math.random().toString(36).substr(2);
+
+let localConnection = new RTCPeerConnection(null, null);
+let dataChannel = localConnection.createDataChannel('dataChannel', null);
+let remoteUser;
+let offer = false;
+
+localConnection.onicecandidate = (event) => {
+    console.log('on ice candidate event');
+    if (event.candidate) {
+        webSocket.send(JSON.stringify({
+            action: 'exchangeCandidate',
+            content: {
+                candidate: event.candidate,
+                target: remoteUser,
+                from: name
+            },
+        }))
+    }
+}
+
+dataChannel.onopen = () => {
+    console.log('dataChannel open');
+    console.log(messageContainer);
+    messageContainer.disabled = false;
+    dataChannelSendBtn.disabled = false;
+}
+
+dataChannel.onmessage = (data) => {
+    console.log('dataChannel receive data');
+    console.log(data);
+}
+
+dataChannel.onclose = () => {
+    console.log('dataChannel close');
+    messageContainer.disabled = true;
+    dataChannelSendBtn.disabled = true;
+}
 
 dataChannelSendBtn.onclick = () => {
+    let message = messageContainer.value;
+    console.log(message);
+    dataChannel.send(message);
+}
 
+localConnection.ondatachannel = (event) => {
+    let channel = event.channel;
+    channel.onopen = () => {
+        console.log('channel open')
+    }
+
+    channel.onmessage = (data) => {
+        console.log('channel receive data')
+        console.log(data);
+    }
 }
 
 webSocket.onopen = () => {
@@ -22,7 +74,10 @@ webSocket.onopen = () => {
         action: "getOnlineUsers",
         content: "all"
     }));
-    dataChannelSendBtn.disabled = false;
+    webSocket.send(JSON.stringify({
+        action: 'login',
+        content: name
+    }))
 };
 
 webSocket.onclose = () => {
@@ -39,15 +94,52 @@ webSocket.onmessage = (data) => {
             case 'exchangeDes':
                 console.log('set remote description');
                 localConnection.setRemoteDescription(message.des);
+                remoteUser = message.from;
+
+                if (!offer) {
+                    localConnection.createAnswer().then((des) => {
+                        console.log('answer set local description');
+                        localConnection.setLocalDescription(des);
+                        webSocket.send(JSON.stringify({
+                            action: 'exchangeDes',
+                            content: {
+                                target: message.from,
+                                des: des,
+                                from: name
+                            }
+                        }))
+                    })
+                }
+                
                 break;
             case 'loginNotice':
-                
+                appendUser(message.content);
+                break;
+            case 'userOffline':
+                let users = loginUsersBox.children;
+                loginUsersBox.childElementCount
+                for (let i = 0; i < loginUsersBox.childElementCount; i++) {
+                    if (users[i].text == message.content) {
+                        loginUsersBox.removeChild(users[i]);
+                    }
+                }
+                break;
+            case 'getOnlineUsers':
+                message.content.forEach(element => {
+                    appendUser(element);
+                });
+                break;
+            case 'exchangeCandidate':
+                console.log('exchange candidate');
+                localConnection.addIceCandidate(
+                    message.candidate
+                ).then(() => {
+                    console.log('addIceCandidate success');
+                }, (err) => {
+                    console.log('failed to add ice candidate:' + err.toString())
+                })
+                break;
         }
-
-    }
-    if (message.action && message.action === 'exchangeDes') {
-        console.log('set remote description');
-        localConnection.setRemoteDescription(message.des);
     }
 };
 
@@ -55,3 +147,25 @@ webSocket.onerror = (err) => {
     console.log('error');
     console.log(err);
 };
+
+function appendUser(username) {
+    let loginUser = document.createElement('wired-item');
+    loginUser.text = username;
+    loginUser.value = username;
+    loginUser.onclick = () => {
+        localConnection.createOffer().then((des) => {
+            offer =true;
+            remoteUser = username;
+            localConnection.setLocalDescription(des);
+            webSocket.send(JSON.stringify({
+                action: 'exchangeDes',
+                content: {
+                    target: username,
+                    des: des,
+                    from: name
+                }
+            }))
+        })
+    }
+    loginUsersBox.appendChild(loginUser);
+}
